f2300364580a9d2e039c45c7861a889a85f809e1
MGAC676 <145212606+MGAC676@users.noreply.github.com>
2023-12-24 22:40:15 -0500

    Add files via upload

---
+++ Main2.py
@@ -0,0 +1,24 @@
+favSongs = ["LA Story", "Lost Stars", "Grenade"]
+favMovies = [" Avengers", "Iron Man", "Hulk"]
+favorites = favSongs + favMovies
+favorites.append("NYPD Red")  # Amazing Book;)
+print("Favorites")
+
+total_people = 20 - 10 + 3 + 15 - 5 + (3 * 15)
+print(total_people)
+
+first_name = input("Enter you first name: ")
+last_name = input("Enter you last name")
+print("Hello thera, %s %s!" % (first_name, last_name))
+
+if 5 > 2:
+    print("My name is carter")
+else:
+    print("the earth has collapsed")
+
+if 2 < 5:  # Modified condition to make it True
+    print("My name is Bob")
+else:
+    print("the earth has collapsed")
+
+    
---
+++ Test3.py
@@ -0,0 +1,56 @@
+def get_choices():
+    player_choice = "rock"
+    computer_choice = "paper"
+    choices = {"player": player_choice, "computer": computer_choice }
+    return computer_choice
+
+
+choices = get_choices()
+print(choices)
+
+dict = {"name":  "beau", "color": choices}
+
+age = 14
+if age > 13:
+    print("You are eligible le for facebook")
+
+
+    # YOU are eligible le for facebook
+
+if age > 13:
+    print("You are eligible le for facebook now?")
+
+
+    #You are eligible le for facebook
+
+    name = "Bob"
+    if age > 13:
+        if name == "Bob":
+            print('You get  Special privileges')
+    else:
+        print("You are eligible for facebook")
+
+
+day = "Tuesday"
+if day == "Monday":
+    print("Sunny!")
+else:
+    print("Rainy")
+
+    #Rainy
+if day == "Monday":
+    print("Sunny!")
+elif day == "Tuseday":
+    print("Coludy!")
+
+else:
+    print("Rainy!")
+
+
+
+
+
+
+
+
+
